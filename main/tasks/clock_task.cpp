063d4cdf6a0269f13a95e3f356b41e78e75bca9b
Robin Werth <robin.werth@gmx.de>
2026-09-06 15:05:14 +0000

    feat: edit clock task to pass structs for date and time

--- main/tasks/clock_task.cpp
+++ main/tasks/clock_task.cpp
@@ -11,13 +11,23 @@
 #include "hardware.h"  // rtc hardware
 #include "ui.h"
 
+static clock_time_t current_time = {0, 0, 0};
+static date_time_t current_date = {0, 0, 0};
+
 void clock_task(void* arg) {
   while (true) {
-    RTC_DateTime dt = rtc.getDateTime();
+    RTC_DateTime date_time = rtc.getDateTime();
+
+    current_time.hours = date_time.getHour();
+    current_time.minutes = date_time.getMinute();
+    current_time.seconds = date_time.getSecond();
+
+    current_date.year = date_time.getYear();
+    current_date.month = date_time.getMonth();
+    current_date.day = date_time.getDay();
 
     bsp_display_lock(0);
-    ui_update_clock(dt.getHour(), dt.getMinute(), dt.getSecond(), dt.getDay(),
-                    dt.getMonth(), dt.getYear());
+    ui_update_clock(&current_time, &current_date);
     bsp_display_unlock();
 
     vTaskDelay(pdMS_TO_TICKS(CLOCK_TASK_PERIOD_MS));
--- main/tasks/clock_task.h
+++ main/tasks/clock_task.h
@@ -6,20 +6,42 @@
  */
 
 #pragma once
+#include <stdint.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+  uint8_t hours;
+  uint8_t minutes;
+  uint8_t seconds;
+} clock_time_t;
+
+typedef struct {
+  uint8_t year;
+  uint8_t month;
+  uint8_t day;
+} date_time_t;
+
 /** @brief FreeRTOS stack depth for the clock task in bytes. */
-static constexpr uint32_t CLOCK_TASK_STACK_DEPTH = 4096;
+#define CLOCK_TASK_STACK_DEPTH 4096
 
 /** @brief FreeRTOS priority for the clock task. */
-static constexpr UBaseType_t CLOCK_TASK_PRIORITY = 5;
+#define CLOCK_TASK_PRIORITY 5
 
 /** @brief Clock display update interval in milliseconds. */
-static constexpr uint32_t CLOCK_TASK_PERIOD_MS = 1000;
+#define CLOCK_TASK_PERIOD_MS 1000
 
 /**
  * @brief FreeRTOS task for updating the clock display every second.
  * @param arg Unused parameter.
  */
 void clock_task(void* arg);
+
+#ifdef __cplusplus
+}
+#endif
