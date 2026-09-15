0184d1be77e448ba998ff699f1497926546299ad
Robin Werth <robin.werth@gmx.de>
2026-09-15 15:58:45 +0000

    style(ui): declare new ui fonts

--- components/ui/fonts/fonts.h
+++ components/ui/fonts/fonts.h
@@ -11,6 +11,10 @@ extern "C" {
 // Custom fonts with full Latin-1 (0x20-0xFF) — includes ä ö ü Ä Ö Ü ß
 LV_FONT_DECLARE(lv_font_montserrat_14_ext);
 LV_FONT_DECLARE(lv_font_montserrat_16_ext);
+LV_FONT_DECLARE(lv_font_montserrat_18);
+LV_FONT_DECLARE(lv_font_montserrat_20_ext);
+LV_FONT_DECLARE(lv_font_montserrat_24);
+LV_FONT_DECLARE(lv_font_montserrat_32_ext);
 LV_FONT_DECLARE(lv_font_montserrat_72_ext);
 LV_FONT_DECLARE(lv_font_montserrat_164);
 LV_FONT_DECLARE(lv_font_shoe_print_48);
