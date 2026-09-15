f5f0eaf313de1b0e4ad2a268d9aab073d956dfa3
Robin Werth <robin.werth@gmx.de>
2026-09-15 16:04:30 +0000

    refactor(ui): change variables to use new battery status struct in ui preview

--- components/ui/ui_preview.c
+++ components/ui/ui_preview.c
@@ -13,9 +13,10 @@ void lvgl_live_preview_init(void) {
 
   clock_time_t time = {14, 30, 00};
   date_time_t date = {2026, 6, 18};
+  battery_status_t battery = {25, true, 3.85f};
 
   ui_update_clock(&time, &date);
-  ui_update_battery(75, false, 3.85f);
+  ui_update_battery(&battery);
   ui_update_steps(1234);
   ui_set_ble_status(true);
   ui_add_notification("WhatsApp", "Hey, wie geht's?");
